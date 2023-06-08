7c1e5225a53735dfb3c7c7ff8087861e705d69a5
지민 백 <s2104@e-mirim.hs.kr>
2023-06-08 10:29:05 +0900

    keep: hover했을때 이미지 src 바뀜

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,18 +1,23 @@
 import { Navbar, Nav, Form, Button, Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/common/Product.css';
+import { useState } from 'react';
 
-export default function Product({ item_id }) {
+export default function Product({ product }) {
+  const [isHover, setIsHover] = useState(false);
+
   return (
     <div className='product'>
-      <a href='#'>
+      <a href='#' 
+        onMouseOver={() => setIsHover(true)}
+        onMouseOut={() => setIsHover(false)}>
         <div className="profile_wrapper">
-          <img className="main" src="https://i.postimg.cc/pdWgDHtN/image.png" alt="item image" />
+          <img className="main" src={isHover ? product.imageUrl[1] : product.imageUrl[0]} alt="item image" />
           {/* <img className="wish" src="/images/wish_empty.png" /> */}
         </div>
-        <p>단하</p>
-        <span>분홍 대슘 원피스</span><br />
-        <span>19,200원</span>
+        <p>{(product.category.title).split('.')[1]}</p>
+        <span>{product.name}</span><br />
+        <span>{product.price}원</span>
       </a>
     </div>
   );
